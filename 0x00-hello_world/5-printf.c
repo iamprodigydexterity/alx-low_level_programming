c20bb64663d39525b9ddad3ddb8aabfbd8ed1d78
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-08 16:46:43 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,12 @@
-#include <stdio.oh>
+#include <stdio.h>
 
 /**
- * main - Printing
+ * main - Entry point
  *
- * Return: Always 0 (Successful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0)
 }
-
-
-
-
-
-
-
-
