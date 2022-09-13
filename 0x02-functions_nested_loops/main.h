cc235d3af5bf4139eb3dfa2df22f24bba163db61
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-13 13:45:28 -0700

    Add

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
-voi print_alphabet_x10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
