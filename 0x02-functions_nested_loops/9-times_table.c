2dad7e505a273b7ad433c1dc349e07a1af556c59
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-13 13:00:47 -0700

    Add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 	for (num = 0; num <= 0; num++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
+		{			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
