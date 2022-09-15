00f574301a8ab1ab9d54dc3e7a8696f7c48aa46c
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-14 18:20:30 -0700

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,4 @@
-#include "main h"
+#include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -30,7 +30,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf(Remaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
