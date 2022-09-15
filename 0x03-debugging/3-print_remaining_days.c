ae0e9cbdec8be09d4e7b4e29baebab765ecdf9a5
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-14 17:53:50 -0700

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,8 +18,8 @@ if (month >= 3 && day >= 60)
 day++;
 }
 
-printf("Day of the year; %d\n", day);
-printf("Remaining days; %d\n", 366 - day);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
