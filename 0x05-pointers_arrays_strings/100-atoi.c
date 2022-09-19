512eab792df9959ad1c7006d7a7d99d97202ca15
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-19 06:34:32 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,13 +18,13 @@ int _atoi(char *s)
 	while (*temp != '\0' && (temp < '0' || *temp > '9'))
         {
 		if (*temp == '-')
-			sign *= -1
+			sign *= -1;
 		temp++;
 	}
         if (*temp != '\0')
         {
 		do {
-			num = num *10 = (*temp - '0');
+			num = num * 10 + (*temp - '0');
 			temp++;
 		} while (*temp >= '0' && *temp <= '9');
 	}
