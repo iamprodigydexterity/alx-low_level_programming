03cb769eb06fdb153119023665ac9b00dc3c854f
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-22 01:33:14 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,13 @@
 /**
  * _strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared
-   @s2: A pointer to the second string to be compared
+ * @s2: A pointer to the second string to be compared
  *
  * Return: If str1 < str2, the negative difference of the first unmatched char
  *         If str1 == str2, 0.
  *         If str1 > str2, the positive difference of the first unmatched char
- */         
- *int _strcmp(char *s1, char *s2)
+ */
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
@@ -17,5 +17,5 @@
 		s2++;
 	}
 
-	return (*s1 - *s2);
+	return (*s1 - *s2)
 }
