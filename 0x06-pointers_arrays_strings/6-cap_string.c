2f4b8736f4418a8bf4acb45d25c7569da7c3a2fe
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-22 07:11:14 -0700

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -32,7 +32,7 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == ' ' ||
-			        str[index - 1] == '\t' ||
+				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
 				str[index - 1] == ';' ||
@@ -44,9 +44,8 @@ char *cap_string(char *str)
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
-			str[index] -+ 32;
+			str[index] -= 32;
 	}
 
 	return (str);
 }
-	
