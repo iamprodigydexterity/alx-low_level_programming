e60123509ad9bcb791a519b8c621bb9894eb6bd0
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-21 17:18:54 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strcmp - Function which compare two strings
- *@s1: first string
- *@s2:second string
- *Return:
- * 		 returns zero if s1 == s2
- * 		 returns negative number if s1 < s2
- * 		 returns positive number if s1 > s2
+ * _strcmp - function which compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return:
+ *                 returns zero if s1 == s2
+ *                 returns negative number if s1 < s2
+ *                 returns positive  number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
+	int i =0, diff = 0;
 
 	while (1)
 	{
@@ -34,7 +34,13 @@ int _strcmp(char *s1, char *s2)
 			break;
 		}
 		else
-			i++:
+			i++;
 	}
 	return (diff);
 }
+
+
+
+
+
+
