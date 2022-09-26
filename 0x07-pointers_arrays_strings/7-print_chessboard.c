5d1b30e1faf8a509dc1299ca0958c68c86d7a866
iamprodigydexterity <oadejugbe404@gmail.com>
2022-09-26 10:56:38 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include "main'h"
+#include "main.h"
 
 /**
  * print_chessboard - prints the chessboard
@@ -14,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(a[i][j];
 		}
 		_putchar('\n');
 	}
