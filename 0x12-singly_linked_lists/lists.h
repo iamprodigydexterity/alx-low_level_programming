40cd202e6911f73004fff88b431e8591e77c9dcd
iamprodigydexterity <oadejugbe404@gmail.com>
2022-10-21 11:56:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,9 +7,9 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: points to the next mode
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
@@ -24,6 +24,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list-t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *str);
 
