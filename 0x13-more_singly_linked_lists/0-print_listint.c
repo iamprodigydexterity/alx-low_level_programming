9ddd315bca8ed5445c50ad302d572c9435f32c2c
iamprodigydexterity <oadejugbe404@gmail.com>
2022-10-24 15:25:09 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t files
+ * print_listint - Prints all the elements of a listint_t list
  * @h: A pointer to the head of the list_t list
  *
  * Return: The number of nodes in the list_t list
