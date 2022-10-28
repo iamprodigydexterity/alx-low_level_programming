26424afc4e22b6122b39dc259fe65a16215b0221
iamprodigydexterity <oadejugbe404@gmail.com>
2022-10-27 22:43:49 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-
-get_endianness - checks whether a machine is big endian or little
-
-Return: 1 if little endian, 0 otherwise
 /
+* get_endianness - checks whether a machine is big endian or little
+*
+* Return: 1 if little endian, 0 otherwise
+* /
 int get_endianness(void)
 {
 	unsigned int test = 1;
